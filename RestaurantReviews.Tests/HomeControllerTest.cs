3577ae7932517ce0b97a751ea663d3f5a6a5e2f3
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-13 22:15:20 +0200

    Add About action in HomeController with unit tests.
    Also, AboutModel is added to Models folder.

---
+++ RestaurantReviews.Tests/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantReviews.Controllers;
+using System.Web.Mvc;
+
+namespace RestaurantReviews.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void AboutTest()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+        }
+    }
+}
---
+++ RestaurantReviews/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using RestaurantReviews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantReviews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            AboutModel model = new AboutModel()
+            {
+                Name = "Restaurant Reviews",
+                Location = "Toronto, CA"
+            };
+
+            return View(model);
+        }
+    }
+}
