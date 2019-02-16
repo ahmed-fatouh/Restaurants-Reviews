a666c5c85bba70a61cc472b1cafe6c317b53f744
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-16 20:48:20 +0200

    Add Index and Details actions into CuisineController

---
+++ RestaurantReviews/Controllers/CuisineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantReviews.Controllers
+{
+    public class CuisineController : Controller
+    {
+
+        [ActionName("All")]
+        public ActionResult Index(string name)
+        {
+            return Content("<<< Index|All action -> lists all cuisines >>>");
+        }
+
+        public ActionResult Details(string name)
+        {
+            return Json(new { Name = name, Action = "Details" }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
