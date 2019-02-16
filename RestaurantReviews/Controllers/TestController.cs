8267f26ec72331157242e033b70db2fce0bb9d68
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-16 17:38:08 +0200

    Add RouteValues and QueryStringValues actions to TestController

---
+++ RestaurantReviews/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantReviews.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult RouteValues()
+        {
+            return View(RouteData.Values);
+        }
+
+        public ActionResult QueryStringValues()
+        {
+            return View(Request.QueryString);
+        }
+    }
+}
