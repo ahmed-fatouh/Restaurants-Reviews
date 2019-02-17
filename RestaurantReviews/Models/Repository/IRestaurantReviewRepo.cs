dff2958fa909cec8d66ebb7399fae4abc6179743
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-17 14:56:44 +0200

    Add RestaurantReview class and repository

---
+++ RestaurantReviews/Models/Repository/IRestaurantReviewRepo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantReviews.Models.Repository
+{
+    public interface IRestaurantReviewRepo
+    {
+        List<RestaurantReview> Reviews { get; }
+
+        void AddReview(RestaurantReview review);
+        void DeleteReview(int id);
+        RestaurantReview GetReview(int id);
+        void UpdateReview(RestaurantReview review);
+    }
+}
---
+++ RestaurantReviews/Models/Repository/RestaurantReviewRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReviews.Models.Repository
+{
+    public class RestaurantReviewRepo : IRestaurantReviewRepo
+    {
+        private static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Cinnamon Club",
+                Country = "UK",
+                City = "London",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 2,
+                Name = "Marrakesh",
+                Country = "D.C.",
+                City = "USA",
+                Rating = 10
+            },
+            new RestaurantReview
+            {
+                Id = 3,
+                Name = "The House of Eliot",
+                Country = "Ghent",
+                City = "Belgium",
+                Rating = 10
+            }
+        };
+
+        public List<RestaurantReview> Reviews => _reviews;
+
+        public RestaurantReview GetReview(int id) => _reviews.Find(r => r.Id == id);
+
+        public void AddReview(RestaurantReview review) => _reviews.Add(review);
+
+        public void UpdateReview(RestaurantReview review)
+        {
+            _reviews.RemoveAt(review.Id);
+            _reviews.Add(review);
+        }
+
+        public void DeleteReview(int id) => _reviews.RemoveAt(id);
+    }
+}
