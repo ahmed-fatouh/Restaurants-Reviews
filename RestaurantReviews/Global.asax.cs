d69c814fd6ea98cf4bf8804d3f4d873f734932fd
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-17 22:42:02 +0200

    Add Filter Configuration with error view

--- RestaurantReviews/Global.asax.cs
+++ RestaurantReviews/Global.asax.cs
@@ -13,6 +13,7 @@ namespace RestaurantReviews
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
