3c302df579d59d4c29ea63b33ea216c6a3597434
ahmed-fatouh <39876806+ahmed-fatouh@users.noreply.github.com>
2019-02-20 22:17:01 +0200

    Add RestaurantReviewDbRepo class as an access layer to the database

---
+++ RestaurantReviews/Models/Repository/RestaurantReviewDbRepo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReviews.Models.Repository
+{
+    public class RestaurantReviewDbRepo : IRestaurantReviewRepo
+    {
+        private RestaurantReviewsDb db = new RestaurantReviewsDb();
+
+        public List<RestaurantReview> Reviews => db.Reviews.ToList();
+
+        public RestaurantReview GetReview(int id) => db.Reviews.Find(id);
+
+        public List<RestaurantReview> GetRestaurantReviews(int restaurantId) =>
+            db.Reviews.Where(r => r.RestaurantId == restaurantId).ToList();
+
+        public void AddReview(RestaurantReview review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+        }
+
+        public void UpdateReview(RestaurantReview review)
+        {
+            RestaurantReview reviewToUpdate = GetReview(review.Id);
+            if (reviewToUpdate == null)
+                throw new Exception("Review not found");
+            reviewToUpdate.Name = review.Name;
+            reviewToUpdate.Body = review.Body;
+            reviewToUpdate.Rating = review.Rating;
+            db.SaveChanges();
+        }
+
+        public void DeleteReview(int id)
+        {
+            db.Reviews.Remove(new RestaurantReview { Id = id });
+            db.SaveChanges();
+        }
+    }
+}
